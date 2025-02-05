6d139f4866c382469a961cf2b4ffe0c11801af18
fairytaib <fairytaib@gmail.com>
2025-02-05 18:52:21 +0100

    Save

---
+++ assets/javascript/script.js
@@ -0,0 +1,155 @@
+const startPlayerDisplay = document.getElementById("player-name-display")
+const addPlayerButton = document.getElementById("add-player-button")
+const playerInputDisplay = document.getElementById("player-input")
+const startButtonSection = document.getElementById("start-button-section")
+const playerNameInput = document.getElementById("player-name-input")
+const playerDisplayTitle = document.getElementById("player-name-display-title")
+const playerNameDisplay = document.getElementById("player-name-display")
+const startButton = document.getElementById("start-game-button")
+
+
+const tasks = ["Namen mit A", "Namen mit B", "Namen mit C"]
+let playerList =[]
+let playerCounter = 0
+let roundCounter = 0
+
+class Player{
+    constructor(name, minusPoints = 0, identifier){
+        this.name = name
+        this.minusPoints = minusPoints
+        this.identifier = identifier
+    }
+}
+    
+function fetchTasks(){
+    let randomTask = Math.floor(Math.random() * tasks.length)
+    return tasks[randomTask]
+}
+
+function fetchRoundTime(){
+    const min = 12
+    const max = 45
+    return randomTime = Math.floor(Math.random() * (max-min + 1)) + min
+}
+
+function createPlayer(name){
+    const newPlayer = new Player(name, 0, playerCounter++);
+    playerList.push(newPlayer);
+}
+
+function displayPlayer(){
+    const playerNameButton = document.createElement("button");
+    const player = playerList[playerList.length - 1]; // Letzten Spieler holen
+    playerNameButton.classList.add("player-name-button");
+    playerNameButton.innerText = `${player.name} X`;
+    playerNameInput.value = ""
+    startPlayerDisplay.appendChild(playerNameButton)
+
+    playerNameButton.addEventListener("click", (e) => { 
+        const index = playerList.findIndex(p => p.identifier === player.identifier);
+        if (index > -1) {
+            playerList.splice(index, 1);
+        }
+        e.target.remove();
+    })
+}
+
+
+function removeContent(){
+    startPlayerDisplay.innerHTML = ""
+    playerInputDisplay.innerHTML = ""
+    startButtonSection.innerHTML = ""
+}
+
+function displayTask(){
+    let task = fetchTasks()
+    startPlayerDisplay.innerText = task
+}
+
+function createSound(sound, id){
+    const existingAudio = document.getElementById(id);
+    if (!existingAudio) {
+        const audio = document.createElement("audio");
+        audio.id = id
+        audio.src = `assets/sounds/${sound}`;
+        audio.autoplay = true;
+        audio.volume = 0.25;
+        if (id === "ticking-sound"){
+            audio.loop = true;
+        } else {
+            audio.loop = false;
+        }
+        document.body.appendChild(audio);
+        audio.play().catch(error => console.error("Error to play sound:", error));
+    }
+}
+
+function removeSound(){
+    const existingAudio = document.getElementById("ticking-sound");
+    existingAudio.remove();
+}
+
+function displayPlayerNames(){
+    for (let i = 0; i < playerList.length; i++){
+        const playerNameButton = document.createElement("button");
+        const player = playerList[i];
+        playerNameButton.classList.add("player-name-button");
+        playerNameButton.innerText = `${player.name} X`;
+        playerNameInput.value = ""
+        startPlayerDisplay.appendChild(playerNameButton)
+
+        playerNameButton.addEventListener("click", (e) => {
+            player.minusPoints += 1;
+            removeContent;
+        })
+    }
+}
+
+
+function goToLosingScreen(){
+    removeContent();
+    removeSound("ticking-sound");  // Stoppt vorherigen Sound
+    createSound("explosion.mp3", "explosion-sound");
+    const whoLostTitle = document.createElement("h2");
+    whoLostTitle.innerText = "Wer hat verloren?";
+    startPlayerDisplay.appendChild(whoLostTitle);
+}
+
+
+function startGame(){
+    let roundTime = fetchRoundTime()
+    removeContent()
+    displayTask()
+    createSound("click-sound.mp3", "ticking-sound")
+    setTimeout(() => {
+        goToLosingScreen
+    }, roundTime)
+}
+
+
+addPlayerButton.addEventListener("click", () =>{
+    const pattern = /^[a-zA-Z0-9]+$/;
+    playerDisplayTitle.remove()
+    playerNameDisplay.style.display = "block"
+    const x  = playerNameInput.value
+    if (x.length < 2){
+        playerNameInput.value = ""
+        playerNameInput.placeholder = "Bitte mindestens 2 Buchstaben verwenden"
+        playerNameInput.style.setProperty("--placeholder-color", "red"); 
+    } else if (!pattern.test(x)){
+        playerNameInput.value = "";
+        playerNameInput.placeholder = "Bitte nur Buchstaben benutzen"; 
+        playerNameInput.style.setProperty("--placeholder-color", "red");
+    } else if (x.length >= 20) {
+        playerNameInput.value = "";
+        playerNameInput.placeholder = "Digga chill mal. Das ist zu lange"; // Error message for long names
+        playerNameInput.style.setProperty("--placeholder-color", "red");
+    } else {
+        createPlayer(x)
+        displayPlayer()
+    }
+})
+
+startButton.addEventListener("click", () => {
+    startGame()
+})
