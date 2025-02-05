d40e8ee15a63893a87ce0b869eb5b40241517f0f
fairytaib <fairytaib@gmail.com>
2025-02-05 16:38:38 +0100

    :)

--- basis-code.js
+++ basis-code.js
@@ -1,6 +1,10 @@
 const startPlayerDisplay = document.getElementById("player-name-display")
 const addPlayerButton = document.getElementById("add-player-button")
-const playernameInput = document.getElementById("player-name-input")
+const playerNameInput = document.getElementById("player-name-input")
+const playerDisplayTitle = document.getElementById("player-name-display-title")
+const playerNameDisplay = document.getElementById("player-name-display")
+
+
 const tasks = []
 let playerList =[]
 let playerCounter = 0
@@ -35,30 +39,40 @@ function displayPlayer(){
     const playerId = playerList[playerCounter].identifier
     playerNameButton.classList.add("player-name-button");
     playerNameButton.innerText = `${playerList[playerCounter].name} X`;
-    playernameInput.value = ""
+    playerNameInput.value = ""
     startPlayerDisplay.appendChild(playerNameButton)
     playerCounter++
 
     playerNameButton.addEventListener("click", (e) => { 
-        const index = playerList.findIndex(player => player.identifier === playerCounter);
+        const index = playerList.findIndex(player => player.identifier === playerId);
         if (index > -1) {
             playerList.splice(index, 1);
-        }
+    }
 
-        e.target.remove();
-        playerCounter--
+    e.target.remove();
     })
 }
 
 
 addPlayerButton.addEventListener("click", () =>{
-    const x  = playernameInput.value
-    createPlayer(x)
-    displayPlayer()
+    const pattern = /^[a-zA-Z0-9]+$/;
+    playerDisplayTitle.remove()
+    playerNameDisplay.style.display = "block"
+    const x  = playerNameInput.value
+    if (x.length <= 2){
+        playerNameInput.value = ""
+        playerNameInput.placeholder = "Bitte mindestens 2 Buchstaben verwenden"
+        playerNameInput.style.setProperty("--placeholder-color", "red"); 
+    } else if (!pattern.test(x)){
+        playerNameInput.value = "";
+        playerNameInput.placeholder = "Bitte nur Buchstaben benutzen"; 
+        playerNameInput.style.setProperty("--placeholder-color", "red");
+    } else if (x.length >= 20) {
+        playerNameInput.value = "";
+        playerNameInput.placeholder = "Digga chill mal. Das ist zu lange"; // Error message for long names
+        playerNameInput.style.setProperty("--placeholder-color", "red");
+    } else {
+        createPlayer(x)
+        displayPlayer()
+    }
 })
-
-
-
-
-
-
