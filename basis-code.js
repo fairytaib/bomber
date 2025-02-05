05cdc0e2ae19565d6c29b733701688ae723c9e56
fairytaib <fairytaib@gmail.com>
2025-02-05 14:22:23 +0100

    Add starting logic to remove buttons and their ids

--- basis-code.js
+++ basis-code.js
@@ -3,11 +3,13 @@ const addPlayerButton = document.getElementById("add-player-button")
 const playernameInput = document.getElementById("player-name-input")
 const tasks = []
 let playerList =[]
+let playerCounter = 0
 
 class Player{
-    constructor(name, minusPoints = 0){
+    constructor(name, minusPoints = 0, identifier){
         this.name = name
         this.minusPoints = minusPoints
+        this.identifier = identifier
     }
 }
     
@@ -22,20 +24,30 @@ function fetchRoundTime(){
     return randomTime = Math.floor(Math.random() * (max-min + 1)) + min
 }
 
-
-
 function createPlayer(name){
-    const newplayer = new Player(name)
+    const newplayer = new Player(name, playerCounter)
     playerList.push(newplayer)
 }
 
 
-function displayPlayer(){ 
+function displayPlayer(){
     const playerNameButton = document.createElement("button");
+    const playerId = playerList[playerCounter].identifier
     playerNameButton.classList.add("player-name-button");
-    playerNameButton.innerText = `${playerList[0].name} X`;
+    playerNameButton.innerText = `${playerList[playerCounter].name} X`;
     playernameInput.value = ""
     startPlayerDisplay.appendChild(playerNameButton)
+    playerCounter++
+
+    playerNameButton.addEventListener("click", (e) => { 
+        const index = playerList.findIndex(player => player.identifier === playerCounter);
+        if (index > -1) {
+            playerList.splice(index, 1);
+        }
+
+        e.target.remove();
+        playerCounter--
+    })
 }
 
 
@@ -44,5 +56,9 @@ addPlayerButton.addEventListener("click", () =>{
     createPlayer(x)
     displayPlayer()
 })
+
+
+
+
 
 
